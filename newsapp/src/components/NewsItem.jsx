f3bca8fbeff21a9e096ef71edc7b32267d31c759
Khem Raj Budhathoki <khemverse@gmail.com>
2026-02-21 11:05:24 +0545

    Video 26: render news cards from sample API data

--- newsapp/src/components/NewsItem.jsx
+++ newsapp/src/components/NewsItem.jsx
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    const { imgUrl, title, description } = this.props;
+    const { imageUrl, newsUrl, title, description } = this.props;
     return (
       <div className="card" style={{ width: "22rem" }}>
-        <img src={imgUrl} className="card-img-top" alt="..." />
+        <img src={imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href="#" className="btn btn-primary">
-            Read News
+          <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">
+            Read More &rarr;
           </a>
         </div>
       </div>
