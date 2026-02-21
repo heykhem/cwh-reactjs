236410cc93913429bcaf0a254cb7d648c5461161
Khem Raj Budhathoki <khemverse@gmail.com>
2026-02-21 09:40:28 +0545

    Video 24: setup News and NewsItem components

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -4,16 +4,31 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is News Component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container mt-5 pb-3">
+        <h1 className="mb-3 fs-3">NewsMonkey - Top Headlines</h1>
+        <div className="row mt-2">
+          <div className="col-md-4">
+            <NewsItem
+              imgUrl="https://www.politico.com/dims4/default/resize/1200/quality/90/format/jpg?url=https%3A%2F%2Fstatic.politico.com%2F08%2F13%2Fa7a9d5234692b1d68ffafae96bb3%2Fgettyimages-2241266904.jpg"
+              title="AMC Theatres Will Refuse to Screen AI Short Film After Online Uproar - The Hollywood Reporter"
+              description="The veterans argued the project would encroach on the nearby Arlington National Cemetery."
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              imgUrl="https://www.politico.com/dims4/default/resize/1200/quality/90/format/jpg?url=https%3A%2F%2Fstatic.politico.com%2F08%2F13%2Fa7a9d5234692b1d68ffafae96bb3%2Fgettyimages-2241266904.jpg"
+              title="AMC Theatres Will Refuse to Screen AI Short Film After Online Uproar - The Hollywood Reporter"
+              description="The veterans argued the project would encroach on the nearby Arlington National Cemetery."
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              imgUrl="https://www.politico.com/dims4/default/resize/1200/quality/90/format/jpg?url=https%3A%2F%2Fstatic.politico.com%2F08%2F13%2Fa7a9d5234692b1d68ffafae96bb3%2Fgettyimages-2241266904.jpg"
+              title="AMC Theatres Will Refuse to Screen AI Short Film After Online Uproar - The Hollywood Reporter"
+              description="The veterans argued the project would encroach on the nearby Arlington National Cemetery."
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- newsapp/src/components/NewsItem.jsx
+++ newsapp/src/components/NewsItem.jsx
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>This is a news item</div>;
+    const { imgUrl, title, description } = this.props;
+    return (
+      <div className="card" style={{ width: "22rem" }}>
+        <img src={imgUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="#" className="btn btn-primary">
+            Read News
+          </a>
+        </div>
+      </div>
+    );
   }
 }
