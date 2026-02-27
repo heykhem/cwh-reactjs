9e7794e0e21deaf0ca20dd71a0da5c91a5d8ec18
Khem Raj Budhathoki <khemverse@gmail.com>
2026-02-27 10:37:31 +0545

    Video 50: add login endpoint and return auth token

--- iNotebook/backend/routes/auth.js
+++ iNotebook/backend/routes/auth.js
@@ -52,7 +52,50 @@ router.post(
       res.status(200).json({ authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal Server Error");
+    }
+  },
+);
+
+// authenticate a user using: POST "api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //if there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user)
+        return res
+          .status(400)
+          .json({ error: "please try to login with correct credentials" });
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare)
+        return res
+          .status(400)
+          .json({ error: "please try to login with correct credentials" });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = await jwt.sign(data, JWT_SECRET);
+      res.status(200).json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
   },
 );
