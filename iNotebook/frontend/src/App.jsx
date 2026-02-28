065f4499ae54543a634717715740fe6fdf375bcc
Khem Raj Budhathoki <khemverse@gmail.com>
2026-02-28 20:20:11 +0545

    Video 62: add Font Awesome icon

--- iNotebook/frontend/src/App.jsx
+++ iNotebook/frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import NoteState from "./context/notes/NoteState.jsx";
+import Alert from "./components/Alert.jsx";
 
 function App() {
   return (
     <>
       <NoteState>
         <Navbar />
+        <Alert message={"note added"} />
         <div className="container mt-3">
           <Outlet />
         </div>
--- iNotebook/frontend/src/components/NoteItem.jsx
+++ iNotebook/frontend/src/components/NoteItem.jsx
@@ -6,6 +6,10 @@ function NoteItem(props) {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <div className="d-flex gap-3 mt-3">
+            <i class="fa-regular fa-pen-to-square"></i>
+            <i class="fa-regular fa-trash-can"></i>
+          </div>
         </div>
       </div>
     </div>
