17fe9c817edb9892b8515d3060e33adecd95cc18
Khem Raj Budhathoki <khemverse@gmail.com>
2026-03-14 09:43:28 +0545

    Video 74: setup reduxapp

---
+++ reduxapp/src/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div>
+      <h1>Buy Adidas Shoes 50 RS</h1>
+      <button className="btn btn-primary">-</button>
+      Add to cart
+      <button className="btn btn-primary">+</button>
+    </div>
+  );
+}
+
+export default Shop;
