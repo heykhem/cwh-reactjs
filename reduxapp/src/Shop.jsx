675e526976fa9d26f9bdfeef68151be07a45ac75
Khem Raj Budhathoki <khemverse@gmail.com>
2026-03-14 11:40:28 +0545

    Video 78: update state from a different component

--- reduxapp/src/Shop.jsx
+++ reduxapp/src/Shop.jsx
@@ -1,10 +1,50 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
+
 function Shop() {
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch,
+  );
+
+  const amount = useSelector((state) => state.amount);
+
   return (
     <div>
       <h1>Deposit/Withdraw Money</h1>
-      <button className="btn btn-primary me-2">-</button>
+      {/* <button
+        className="btn btn-primary me-2"
+        onClick={() => dispatch(actionCreators.withdrawMoney(100))}
+      >
+        -
+      </button>
       Update Balance
-      <button className="btn btn-primary ms-2">+</button>
+      <button
+        className="btn btn-primary ms-2"
+        onClick={() => dispatch(actionCreators.depositMoney(100))}
+      >
+        +
+      </button> */}
+      <button
+        className="btn btn-primary me-2"
+        disabled={amount < 1}
+        onClick={() => {
+          withdrawMoney(100);
+        }}
+      >
+        -
+      </button>
+      Update Balance
+      <button
+        className="btn btn-primary ms-2"
+        onClick={() => {
+          depositMoney(100);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
